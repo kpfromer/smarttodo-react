f2dd226f5f9a3a78965404e3a0f1b377df083cd9
Kyle Pfromer <kpfromer2@gmail.com>
2018-06-03 12:58:08 -0600

    Add starter styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './normalize.scss';
+import './reset.scss';
+import './index.scss';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
