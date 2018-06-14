83f1610a3adfbad579053236c299e14e3ce19b83
Kyle Pfromer <kpfromer2@gmail.com>
2018-06-14 08:41:12 +0200

    Add jest testing packages

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,7 @@
 import Enzyme from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
+import {createSerializer} from 'enzyme-to-json';
+import 'jest-enzyme';
 // Setup enzyme's react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
