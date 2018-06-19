9050982cfeb0c42ddd966b4605f2b446e73ea6bd
Kyle Pfromer <kpfromer2@gmail.com>
2018-06-19 07:27:20 +0200

    Add routing and 404 page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from "react-router-dom";
-
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 //styles
 import './App.scss';
 
 import { Header } from "./components/Header/Header";
+import TodoList from "./containers/TodoList/TodoList";
+import NotFound from "./components/NotFound/NotFound";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <Fragment>
           <Header />
-        </div>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={TodoList} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </Fragment>
       </BrowserRouter>
     );
   }
