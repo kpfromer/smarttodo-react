0e8a1045af8bb5223c6dd5c8a793fc285e17dc31
Kyle Pfromer <kpfromer2@gmail.com>
2018-06-22 21:51:49 -0600

    Fix NotFound invalid style

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,7 +5,7 @@ import styles from './NotFound.module.scss';
 export default class NotFound extends PureComponent {
   render() {
     return (
-      <div style={styles.container}>
+      <div className={styles.container}>
         <h1 className={styles.title}>Page Not Found</h1>
       </div>
     );
