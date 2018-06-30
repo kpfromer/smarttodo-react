15fae5ae2df5b2bfa87c68ece136f14a19fc3a10
Kyle Pfromer <kpfromer2@gmail.com>
2018-06-30 14:40:56 -0700

    Add PrivateRoute

---
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,27 @@
+import React, { PureComponent } from 'react';
+import { Redirect, Route } from "react-router-dom";
+import { connect } from "react-redux";
+
+export class PrivateRoute extends PureComponent {
+
+  render() {
+    // TODO: handle render
+    const { component: Component, isAuthenticated, ...rest } = this.props;
+    return (
+      <Route {...rest} render={(props) => (
+        isAuthenticated
+          ? <Component {...props} />
+          : <Redirect to={{
+            pathname: '/login',
+            // state: { from: props.location }
+          }} />
+      )} />
+    );
+  }
+}
+
+export const mapStateToProps = state => ({
+  isAuthenticated: !!state.login.token
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
---
+++ src/components/PrivateRoute/PrivateRoute.test.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { SetupComponent } from "react-component-setup";
+import { PrivateRoute } from "./PrivateRoute";
+
+const { shallow: setup } = SetupComponent({
+  Component: PrivateRoute
+});
+
+const CustomComponent = () => (
+  <h1>hello world</h1>
+);
+
+describe('PrivateRoute', () => {
+  let component, routerProps;
+
+  beforeEach(() => {
+    component = CustomComponent;
+    routerProps = {
+      hello: 'world',
+      my: 'name is kyle'
+    };
+  });
+
+  it('renders without crashing', () => {
+    setup();
+  });
+
+  it('renders a route to component if `isAuthenticated`', () => {
+    const { wrapper } = setup({
+      component,
+      isAuthenticated: true
+    });
+
+    expect(wrapper.props().render(routerProps)).toMatchSnapshot();
+  });
+
+  it('renders a route that redirects users to `/login` if not `isAuthenticated`', () => {
+    const { wrapper } = setup({
+      component,
+      isAuthenticated: false
+    });
+
+    expect(wrapper.props().render(routerProps)).toMatchSnapshot();
+  });
+
+  it('passes the rest of it\'s props to the Route', () => {
+    const { wrapper } = setup({
+      component,
+      isAuthenticated: false,
+      otherProps: 'needed',
+      to: '/an-example-location'
+    });
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
