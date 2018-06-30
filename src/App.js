517785d147fbb181fc93b0a111e699c8021118e0
Kyle Pfromer <kpfromer2@gmail.com>
2018-06-30 15:02:06 -0700

    Update App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-//styles
-import './App.scss';
 
-import { Header } from "./components/Header/Header";
+import Header from "./components/Header/Header";
 import TodoList from "./containers/TodoList/TodoList";
 import NotFound from "./components/NotFound/NotFound";
+import Login from "./components/Login/Login";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import Logout from "./components/Logout/Logout";
+
+import styles from './App.module.scss'
 
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Fragment>
+        <div className={styles.container}>
           <Header/>
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={TodoList} />
-              <Route exact path="/logout" component={Logout} />
-              <Route component={NotFound}/>
-            </Switch>
-          </div>
-        </Fragment>
+          <Switch>
+            <PrivateRoute path="/todo" component={TodoList}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/logout" component={Logout}/>
+            <Route component={NotFound}/>
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import App from './App';
+import { SetupComponent } from "react-component-setup";
+import Header from "./components/Header/Header";
+import { Switch } from "react-router-dom";
+
+const { shallow: setup } = SetupComponent({
+  Component: App
+});
 
 describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    ({ wrapper } = setup());
+  });
+
   it('renders without crashing', () => {
-    shallow(<App />);
+    setup();
+  });
+
+  it('renders a header', () => {
+    expect(wrapper.find(Header)).toExist();
+  });
+
+  it('renders routes', () => {
+    expect(wrapper.find(Switch)).toMatchSnapshot();
   });
 });
