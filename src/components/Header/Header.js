b363c6658b002fa130ed7c6656a61bda88683a3f
Kyle Pfromer <kpfromer2@gmail.com>
2018-07-01 17:55:37 -0600

    Update Header for material and authentication

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,54 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
 import { NavLink } from "react-router-dom";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import styles from './Header.module.scss';
 
-export const Header = () =>
-  <div className={styles.mainContainer}>
-    <div className={`container ${styles.navContainer}`}>
-      <h1 className={styles.title}>
-        <NavLink to="/">Smarttodo</NavLink>
-      </h1>
-      <ul className={styles.navList}>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/settings">Settings</NavLink>
-      </ul>
-    </div>
-  </div>;
+export const Header = ({ isAuthenticated }) => {
+
+  const menuItems =
+    isAuthenticated ?
+      <NavLink id="logout" to="/logout">
+        <Button color="inherit">
+          Logout
+        </Button>
+      </NavLink>
+      :
+      <NavLink id="login" to="/login">
+        <Button color="inherit">
+          Login
+        </Button>
+      </NavLink>;
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton className={styles.menuButton} color="inherit" aria-label="Menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="title" color="inherit" className={styles.flex}>
+          <NavLink to="/">Smarttodo</NavLink>
+        </Typography>
+        {menuItems}
+      </Toolbar>
+    </AppBar>
+  )
+};
+
+Header.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired
+};
+
+export const mapStateToProps = state => ({
+  isAuthenticated: !!state.login.token
+});
+
+export default connect(mapStateToProps)(Header);
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { SetupComponent } from 'react-component-setup';
-import { Header } from "./Header";
+import { Header, mapStateToProps } from "./Header";
+import AppBar from '@material-ui/core/AppBar';
 
 const { shallow: setup } = SetupComponent({
   Component: Header,
-  elementsToFind: [
-    {
-      name: 'title',
-      query: 'h1'
-    },
-    {
-      name: 'list',
-      query: 'ul'
-    }
-  ]
+  defaultProps: {
+    isAuthenticated: false
+  }
 });
 
 describe("Header", () => {
@@ -27,13 +21,37 @@ describe("Header", () => {
     setup();
   });
 
-  it('renders a title', () => {
-    const { title } = items;
-    expect(title).toMatchSnapshot();
+  it('renders a material design AppBar', () => {
+    const { wrapper } = setup();
+    expect(wrapper.find(AppBar)).toMatchSnapshot();
   });
 
-  it('renders a menu bar', () => {
-    const { list } = items;
-    expect(list).toMatchSnapshot();
+  it('renders a logout link when the user is logged in', () => {
+    const { wrapper } = setup({
+      isAuthenticated: true
+    });
+
+    expect(wrapper.find('#logout')).toMatchSnapshot();
+  });
+  it('renders a login link when the user is not logged in', () => {
+    const { wrapper } = setup({
+      isAuthenticated: false
+    });
+
+    expect(wrapper.find('#login')).toMatchSnapshot();
+  });
+});
+
+describe('mapStateToProps', () => {
+  it('make login token to a boolean `isAuthenticated` value', () => {
+    const state = {
+      login: {
+        token: 'jwt-token'
+      }
+    };
+    const expected = {
+      isAuthenticated: true
+    };
+    expect(mapStateToProps(state)).toEqual(expected);
   });
 });
