29593bad06ed7474709b20fc77e8a5b6f5956414
Kyle Pfromer <kpfromer2@gmail.com>
2018-07-01 10:49:15 -0600

    Add login actiontypes

---
+++ src/actiontypes/login.js
@@ -0,0 +1,4 @@
+export const INVALIDATE_LOGIN = 'login/INVALIDATE_LOGIN';
+export const REQUEST_LOGIN = 'login/REQUEST_LOGIN';
+export const RECEIVE_LOGIN = 'login/RECEIVE_LOGIN';
+export const FAILURE_LOGIN = 'login/FAILURE_LOGIN';
