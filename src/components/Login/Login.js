eb1e6728336fb2bdc38b84c371dc15c6b7c1c9a4
Kyle Pfromer <kpfromer2@gmail.com>
2018-07-02 14:59:28 -0600

    Add Login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,130 @@
+import React, { Fragment, PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Card from '@material-ui/core/Card';
+import { tryLogin } from "../../actions/login";
+import { Redirect, withRouter } from "react-router-dom";
+import Snackbar from "@material-ui/core/Snackbar";
+import { MySnackbarContentWrapper } from "../SpecialSnackbar";
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import FormValidator from "../FormValidator/FormValidator";
+import TextValidator from "../FormValidator/TextValidator";
+
+import styles from './Login.module.scss';
+
+export class Login extends PureComponent {
+
+  static propTypes = {
+    login: PropTypes.func.isRequired
+  };
+
+  state = {
+    username: '',
+    password: '',
+    loginError: false,
+    errorMessage: 'Failed to login',
+    redirectToReferrer: false
+  };
+
+  loginUser = (username, password) =>
+    this.props.login(username, password)
+      .then(
+        () => this.setState({ redirectToReferrer: true })
+      )
+      .catch(repsonse =>
+        this.setState({
+          loginError: true,
+          errorMessage: repsonse.error,
+          password: ''
+        })
+      );
+
+  handleClose = () => this.setState({ loginError: false });
+
+  handleSubmit = () => {
+    const { username, password } = this.state;
+    this.loginUser(username, password);
+  };
+
+  updateValue = valueName => event => {
+    const { value } = event;
+    this.setState({
+      [valueName]: value
+    })
+  };
+
+  validate = value => !!value;
+
+  render() {
+    if (this.state.redirectToReferrer) {
+      // TODO: add flash message to and from
+      // https://github.com/mjrussell/redux-auth-wrapper/issues/64
+      return <Redirect to="/todo"/>;
+    }
+
+    return (
+      <Fragment>
+        <div className={styles.container}>
+          <Card className={styles.card}>
+            <FormValidator onSubmit={this.handleSubmit}>
+              <CardContent>
+                <Typography variant="title">
+                  Login
+                </Typography>
+                <TextValidator
+                  autoFocus
+                  margin="dense"
+                  label="Username"
+                  type="text"
+                  fullWidth
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.updateValue('username')}
+                  validate={this.validate}
+                />
+                <TextValidator
+                  autoFocus
+                  margin="dense"
+                  label="Password"
+                  type="password"
+                  name="password"
+                  value={this.state.password}
+                  fullWidth
+                  onChange={this.updateValue('password')}
+                  validate={this.validate}
+                />
+              </CardContent>
+              <CardActions className={styles.cardActions}>
+                <Button type="submit" size="medium" color="primary">
+                  Submit
+                </Button>
+              </CardActions>
+            </FormValidator>
+          </Card>
+        </div>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          open={this.state.loginError}
+          autoHideDuration={3000}
+          onClose={this.handleClose}
+        >
+          <MySnackbarContentWrapper
+            onClose={this.handleClose}
+            variant="error"
+            message={this.state.errorMessage}
+          />
+        </Snackbar>
+      </Fragment>
+    );
+  }
+}
+
+export default connect(null, {
+  login: tryLogin
+})(withRouter(Login));
---
+++ src/components/Login/Login.test.js
@@ -0,0 +1,129 @@
+import { Login } from "./Login";
+import { SetupComponent } from "react-component-setup";
+import TextValidator from "../FormValidator/TextValidator";
+import FormValidator from "../FormValidator/FormValidator";
+import Snackbar from "@material-ui/core/Snackbar";
+
+const { shallow: setup } = SetupComponent({
+  Component: Login
+});
+
+describe('Login', () => {
+  let wrapper, mockLogin;
+
+  beforeEach(() => {
+    mockLogin = jest.fn();
+    ({ wrapper } = setup({
+      login: mockLogin
+    }));
+  });
+
+  // TODO: dry!
+  describe('user enters username', () => {
+    let username, refresh, newValue;
+
+    beforeEach(() => {
+      refresh = () => wrapper.find(TextValidator).findWhere(node => node.prop('name') === 'username');
+      username = refresh();
+
+      newValue = 'example username';
+
+      username.props().onChange({ value: newValue });
+      wrapper.update();
+      username = refresh();
+    });
+
+    it('username input value changes', () => {
+      expect(username).toHaveProp('value', newValue);
+    });
+    it('validates input', () => {
+      expect(username.props().validate('notempty')).toBe(true);
+      expect(username.props().validate('')).toBe(false);
+    });
+  });
+  
+  describe('user enters password', () => {
+    let password, refresh, newValue;
+
+    beforeEach(() => {
+      refresh = () => wrapper.find(TextValidator).findWhere(node => node.prop('name') === 'password');
+      password = refresh();
+
+      newValue = 'example password';
+
+      password.props().onChange({ value: newValue });
+      wrapper.update();
+      password = refresh();
+    });
+
+    it('password input value changes', () => {
+      expect(password).toHaveProp('value', newValue);
+    });
+    it('validates input', () => {
+      expect(password.props().validate('notempty')).toBe(true);
+      expect(password.props().validate('')).toBe(false);
+    });
+  });
+
+  describe('user submits login form with username and password filled', () => {
+    let username, password, findTextValidatorByName;
+
+    beforeEach(() => {
+      findTextValidatorByName = name => wrapper.find(TextValidator).findWhere(node => node.prop('name') === name);
+
+      username = 'exampleUsername';
+      password = 'examplePassword';
+
+      findTextValidatorByName('username').props().onChange({ value: username });
+      findTextValidatorByName('password').props().onChange({ value: password });
+      wrapper.update();
+    });
+
+    describe('if credentials are valid', () => {
+      beforeEach(async () => {
+        const promise = Promise.resolve();
+        mockLogin.mockReturnValue(promise);
+        wrapper.find(FormValidator).props().onSubmit();
+        await promise;
+        wrapper.update();
+      });
+
+      it('logins in user', () => {
+        expect(mockLogin).toHaveBeenCalledWith(username, password);
+      });
+      it('redirects user to /todo', () => {
+        expect(wrapper).toMatchSnapshot();
+      });
+    });
+
+    describe('if credentials are invalid', () => {
+      let error;
+
+      beforeEach(() => {
+        error = 'Username/password were not found!';
+        const promise = Promise.reject({
+          error
+        });
+        mockLogin.mockReturnValue(promise);
+        wrapper.find(FormValidator).props().onSubmit();
+        return promise
+          .then(() => {}) // See https://github.com/airbnb/enzyme/issues/450#issuecomment-341244926
+          .catch(() => wrapper.update());
+      });
+
+      it('removes password', () => {
+        expect(findTextValidatorByName('password')).toHaveProp('value', '');
+      });
+      it('displays a snackbar message with the api error', () => {
+        expect(wrapper.find(Snackbar)).toMatchSnapshot();
+      });
+      describe('error snackbar', () => {
+        it('closes when the X icon is clicked', () => {
+          wrapper.find(Snackbar).props().onClose();
+          wrapper.update();
+          expect(wrapper.find(Snackbar)).toHaveProp('open', false);
+        });
+      });
+    });
+  });
+});
