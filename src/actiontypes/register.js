e76114762c2fcdce6eccfb5dc8a1be4b463acf4e
Kyle Pfromer <kpfromer2@gmail.com>
2018-07-06 16:49:48 -0600

    Add register actiontypes

---
+++ src/actiontypes/register.js
@@ -0,0 +1,4 @@
+
+export const REQUEST_REGISTER = 'register/REQUEST_REGISTER';
+export const RECEIVE_REGISTER = 'register/RECEIVE_REGISTER';
+export const FAILURE_REGISTER = 'register/FAILURE_REGISTER';
