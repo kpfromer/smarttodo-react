596e7d572f36335031428c7d84f1c7c9c2b9a1f5
Kyle Pfromer <kpfromer2@gmail.com>
2018-07-07 17:39:36 -0600

    Update Register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,63 +1,152 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { tryRegister } from "../../actions/register";
+import { connect } from 'react-redux';
 
-export default class Register extends Component {
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import FormValidator from "../FormValidator/FormValidator";
+import { Redirect } from "react-router-dom";
+import TextValidated from "../FormValidator/TextValidated";
+import { closeSnackbar } from "../../actions/snackbar";
+
+export class Register extends Component {
+
+  static propTypes = {
+    register: PropTypes.func.isRequired
+  };
+
+  state = {
+    redirect: false,
+    username: '',
+    password: '',
+    email: '',
+    firstName: '',
+    lastName: ''
+  };
+
+  updateValue = valueName => event => {
+    const { value } = event;
+    this.setState({
+      [valueName]: value
+    })
+  };
+
+  handleSubmit = () => {
+    const { username, password, email, firstName, lastName } = this.state;
+    this.props.register(username, password, email, firstName, lastName)
+      .then(() => {
+        this.props.closeSnackbar({
+          message: 'Redirecting to login',
+          variant: 'success'
+        });
+        this.setState({ redirect: true })
+      })
+      .catch(response =>
+        this.props.closeSnackbar({
+          message: response.error,
+          variant: 'error'
+        })
+      );
+  };
+
   render() {
+    if (this.state.redirect) {
+      return <Redirect to="/login"/>
+    }
+
     return (
-      <Fragment>
-        <div className={styles.container}>
-          <Card className={styles.card}>
-            <FormValidator onSubmit={this.handleSubmit}>
-              <CardContent>
-                <Typography variant="title">
-                  Login
-                </Typography>
-                <TextValidator
-                  autoFocus
-                  margin="dense"
-                  label="Username"
-                  type="text"
-                  fullWidth
-                  name="username"
-                  value={this.state.username}
-                  onChange={this.updateValue('username')}
-                  validate={this.validate}
-                />
-                <TextValidator
-                  autoFocus
-                  margin="dense"
-                  label="Password"
-                  type="password"
-                  name="password"
-                  value={this.state.password}
-                  fullWidth
-                  onChange={this.updateValue('password')}
-                  validate={this.validate}
-                />
-              </CardContent>
-              <CardActions className={styles.cardActions}>
-                <Button type="submit" size="medium" color="primary">
-                  Submit
-                </Button>
-              </CardActions>
-            </FormValidator>
-          </Card>
-        </div>
-        <Snackbar
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-          }}
-          open={this.state.loginError}
-          autoHideDuration={3000}
-          onClose={this.handleClose}
-        >
-          <SpecialSnackbar
-            onClose={this.handleClose}
-            variant="error"
-            message={this.state.errorMessage}
-          />
-        </Snackbar>
-      </Fragment>
+      <div>
+        <Card>
+          <FormValidator onSubmit={this.handleSubmit}>
+            <CardContent>
+              <Typography variant="title">
+                Register
+              </Typography>
+              <TextValidated
+                name="username"
+                hintName="Username"
+                required
+                value={this.state.username}
+                onChange={this.updateValue('username')}
+                textFieldProps={{
+                  autoFocus: true,
+                  margin: 'dense',
+                  label: 'Username',
+                  type: 'text',
+                  fullWidth: true
+                }}
+              />
+              <TextValidated
+                name="password"
+                hintName="Password"
+                required
+                value={this.state.password}
+                onChange={this.updateValue('password')}
+                textFieldProps={{
+                  autoFocus: true,
+                  margin: 'dense',
+                  label: 'Password',
+                  type: 'password',
+                  fullWidth: true
+                }}
+              />
+              <TextValidated
+                name="email"
+                hintName="Email"
+                email
+                value={this.state.email}
+                onChange={this.updateValue('email')}
+                textFieldProps={{
+                  autoFocus: true,
+                  margin: 'dense',
+                  label: 'Email',
+                  type: 'email',
+                  fullWidth: true
+                }}
+              />
+              <TextValidated
+                name="firstName"
+                hintName="First Name"
+                required
+                value={this.state.firstName}
+                onChange={this.updateValue('firstName')}
+                textFieldProps={{
+                  autoFocus: true,
+                  margin: 'dense',
+                  label: 'First Name',
+                  type: 'text',
+                  fullWidth: true
+                }}
+              />
+              <TextValidated
+                name="lastName"
+                hintName="Last Name"
+                required
+                value={this.state.lastName}
+                onChange={this.updateValue('lastName')}
+                textFieldProps={{
+                  autoFocus: true,
+                  margin: 'dense',
+                  label: 'Last Name',
+                  type: 'text',
+                  fullWidth: true
+                }}
+              />
+            </CardContent>
+            <CardActions>
+              <Button type="submit" size="medium" color="primary">
+                Submit
+              </Button>
+            </CardActions>
+          </FormValidator>
+        </Card>
+      </div>
     );
   }
 }
+
+export default connect(null, { register: tryRegister, closeSnackbar })(Register);
