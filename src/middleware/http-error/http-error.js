d25f27e4c91ce2759bd2b7ebd3aa3f51b03011b7
Kyle Pfromer <kpfromer2@gmail.com>
2018-07-16 22:57:55 -0600

    Fix HttpError by removing unused store

--- src/middleware/http-error/http-error.js
+++ src/middleware/http-error/http-error.js
@@ -2,7 +2,7 @@ export const HTTP_ERROR = 'HTTP_ERROR';
 
 // If http error, this finds the thunk for that specific error and calls it with next (dispatch)
 // Used for 401 (Forbidden) where user if forced logged out
-export default thunksForErrors => store => next => action => {
+export default thunksForErrors => () => next => action => {
   const httpError = action[HTTP_ERROR];
 
   if (typeof httpError === 'undefined') {
