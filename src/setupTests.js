2f051795f6e290b4d3b5312f05b15e787d9dd2c5
Kyle Pfromer <kpfromer2@gmail.com>
2018-10-08 21:32:29 -0600

    Revert setupTests snapshot serializer

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,7 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
+import {createSerializer} from 'enzyme-to-json';
 
 configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
