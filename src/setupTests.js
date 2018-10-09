45a24c4dd3226c9052e16911f5f276aecd924502
Kyle Pfromer <kpfromer2@gmail.com>
2018-10-08 18:46:57 -0600

    Refactor snapshotSerializers

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,5 @@
-import Enzyme from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
-import {createSerializer} from 'enzyme-to-json';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
-// Setup enzyme's react adapter
-Enzyme.configure({ adapter: new EnzymeAdapter() });
-expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+
+configure({ adapter: new Adapter() });
